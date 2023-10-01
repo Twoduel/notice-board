8559c347522af57a2f34c9b00daaebdd417b5ffd
KimSujeong <sujeong0619.b@gmail.com>
2023-10-02 02:12:17 +0900

    feat : 게시글 읽기. - 롬복을 활용해 DTO, Entity, Controller리팩터링. - 로깅. - 단일 데이터 조회. - 데이터 목록 조회.

--- notice-board/src/main/java/com/example/noticeboard/entity/Article.java
+++ notice-board/src/main/java/com/example/noticeboard/entity/Article.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 @Entity
 public class Article {
     @Id
@@ -17,18 +21,7 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
+    Article(){
+        //롬복으로 간단히 줄이는 방법 : @NoArgsConstructor
     }
 }
