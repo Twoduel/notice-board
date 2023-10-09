f33980e62ea3da31edbb4baeba9de5300a2450f9
KimSujeong <sujeong0619.b@gmail.com>
2023-10-09 17:05:02 +0900

    feat : 서비스 계층 추가. - 전에 작성한 ArticleApiController의 index(), create(), update(), delete() 메소드에서 컨트롤러와 서비스의 역할을 분업.

--- notice-board/src/main/java/com/example/noticeboard/repository/ArticleRepository.java
+++ notice-board/src/main/java/com/example/noticeboard/repository/ArticleRepository.java
@@ -4,6 +4,7 @@ import com.example.noticeboard.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public interface ArticleRepository extends CrudRepository<Article, Long> {
